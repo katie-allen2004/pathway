e111239590138662dc7fd7a6b60be43439830d76
katie-allen2004 <katie.allen1004@gmail.com>
2025-12-12 22:42:06 -0800

    flutter template

---
+++ pathway/pathway/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Senior Design App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Senior Design App'),
+      ),
+      body: const Center(
+        child: Text('Hello, senior design!'),
+      ),
+    );
+  }
+}

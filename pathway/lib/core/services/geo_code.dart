ce9e753709144d22c4af9488584365074fddcf66
BIDL8588 <lopezbyron8588@gmail.com>
2026-04-02 00:13:31 -0700

    approval/map progress
    - added to map_screen /venue_model/storage_service)
    - lat/lng filled out by entering the address of a venue
    - Whenever a user adds a new venue, the venue needs to be approaved
    - once approved, the venue will show up on the map with a distinguishing color
    -each map pin is based on access score on tags.
    - when you click on a pin, a mini card appears
    - if you click on the card, you are navigated to the venue detail page

--- pathway/lib/core/services/geo_code.dart
+++ pathway/lib/core/services/geo_code.dart
@@ -1,11 +1,34 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:geocoding/geocoding.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:latlong2/latlong.dart'; //latlong2 imports
 
 class GeocodingService {
+  Future<List<Map<String, dynamic>>> getAddressSuggestions(String query) async {
+    if (query.length < 3) return [];
+
+    // this is what we are using, this will users search up their address to autofill coordiantes. 
+    final url = Uri.parse(
+        'https://nominatim.openstreetmap.org/search?q=$query&format=json&limit=5&addressdetails=1');
+
+    try {
+      final response = await http.get(url, headers: {
+        'User-Agent': 'PathwayApp/1.0', 
+      });
+
+      if (response.statusCode == 200) {
+        final List data = json.decode(response.body);
+        return data.cast<Map<String, dynamic>>();
+      }
+    } catch (e) {
+      print("Autocomplete error: $e");
+    }
+    return [];
+  }
+
   static Future<LatLng?> getCoordinatesFromAddress(String address) async {
     try {
       List<Location> locations = await locationFromAddress(address);
-      
       if (locations.isNotEmpty) {
         return LatLng(locations.first.latitude, locations.first.longitude);
       }
